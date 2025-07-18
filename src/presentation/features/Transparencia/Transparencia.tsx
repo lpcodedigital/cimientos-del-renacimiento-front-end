4898bf25766148abb8adadc6744469de034b23d9
Manolo <manolovp94@gmail.com>
2025-07-17 21:44:32 -0600

    Reordenar elementos en la seccion de transparencia en modo mobil

--- src/presentation/features/Transparencia/Transparencia.tsx
+++ src/presentation/features/Transparencia/Transparencia.tsx
@@ -12,9 +12,10 @@ const Fidecomiso: React.FC = () => {
             <div className="transparencia-modern">
                 {/* Bloque 1 */}
                 <div className="transparencia-item">
-                    <img src={reportes} alt="Reporte" className="transparencia-img-modern" />
+                    <img src={reportes} alt="Reporte" className="img-desktop transparencia-img-modern" />
                     <div className="transparencia-text">
                         <h3>Reportes Trimestrales</h3>
+                        <img src={reportes} alt="Reporte" className="img-mobil transparencia-img-modern" />
                         <p>Consulta informes detallados de avances, recursos ejercidos y metas cumplidas. Información actualizada cada trimestre.</p>
                         <a href="https://tusitio.gob.mx/reportes" target="_blank" rel="noopener noreferrer" className="btn-descargar">
                             Ver reportes
@@ -24,14 +25,15 @@ const Fidecomiso: React.FC = () => {
 
                 {/* Bloque 2 (invertido) */}
                 <div className="transparencia-item reverse">
+                    <img src={auditores} alt="Auditoría" className="img-desktop transparencia-img-modern" />
                     <div className="transparencia-text">
                         <h3>Auditorías Públicas</h3>
+                         <img src={auditores} alt="Auditoría" className="img-mobil transparencia-img-modern" />
                         <p>Accede a auditorías realizadas por órganos de fiscalización. Promovemos la rendición de cuentas y el buen uso de los recursos públicos.</p>
                         <a href="https://tusitio.gob.mx/auditorias" target="_blank" rel="noopener noreferrer" className="btn-descargar">
                             Ver auditorías
                         </a>
                     </div>
-                    <img src={auditores} alt="Auditoría" className="transparencia-img-modern" />
                 </div>
             </div>
         </section>
