c350dcf9046f81eff8b6dd8150f308ce0806dada
Manolo <manolovp94@gmail.com>
2025-07-17 21:43:32 -0600

    Apuntar al repositorio de lpcodedigital

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/front-end-cimientos-del-renacimiento/',
+  //base: '/front-end-cimientos-del-renacimiento/', //manolo-94
+  base: '/cimientos-del-renacimiento-front-end/', //lpcodedigital
 })
