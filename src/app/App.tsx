8d62ed0ac7c4509c133d6b64f9e0d69bc62c16f8
Manolo <manolovp94@gmail.com>
2025-07-21 18:04:09 -0600

    Confiurar HasRouter sin basename

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ function App() {
     //   </Routes>
     // </BrowserRouter>
 
-    <HashRouter basename={basename}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/CursosPage" element={<CursosPage />} />
