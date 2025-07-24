9706a47031984c1528fbbde8f242a33985fbdb67
Manolo <manolovp94@gmail.com>
2025-07-23 21:32:07 -0600

    Ajustar Carrusel a una altura maxima de 500px

--- src/presentation/features/Inicio/Inicio.tsx
+++ src/presentation/features/Inicio/Inicio.tsx
@@ -6,33 +6,33 @@ import { FaArrowDown } from 'react-icons/fa';
 import './Inicio.css'
 
 const Inicio: React.FC = () => {
-    return (
+  return (
 
-       <section id="Inicio" className="hero">
-  <div className="carousel-container position-relative">
-    <Carousel>
-      <Carousel.Item>
-        <CarouselImage src={banner} alt="First slide" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <CarouselImage src={banner} alt="Second slide" />
-      </Carousel.Item>
-      <Carousel.Item>
-        <CarouselImage src={banner} alt="Third slide" />
-      </Carousel.Item>
-    </Carousel>
+    <section id="Inicio" className="hero">
+      <div className="carousel-container position-relative">
+        <Carousel>
+          <Carousel.Item>
+            <CarouselImage src={banner} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <CarouselImage src={banner} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <CarouselImage src={banner} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
 
-    {/* Flecha hacia abajo animada */}
-    <a href="#Fideicomiso" className="scroll-down">
-  <FaArrowDown size={30} />
-</a>
+        {/* Flecha hacia abajo animada */}
+        <a href="#Fideicomiso" className="scroll-down">
+          <FaArrowDown size={30} />
+        </a>
 
-  </div>
-</section>
+      </div>
+    </section>
 
 
 
-    );
+  );
 };
 
 export default Inicio;
